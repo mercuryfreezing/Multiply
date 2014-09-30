3f643d18abcf1bb642a1542339089a3ab0b01327
roshan <roshan.prakash.r@gmail.com>
2014-09-30 16:11:39 +0100

    UISegmentControl with self.operatorLabel.selectedSegmentIndex == 0

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -27,33 +27,42 @@
     self.sliderForMultiplier.minimumValue = 0.0;
     self.sliderForMultiplier.maximumValue = 10.0;
 
+    //Converting a integer to a String.
+    int val = self.sliderForMultiplier.value;
+    NSString *stringValue = [NSNumber numberWithInt:val].description;
+    self.multiplierLabel.text = stringValue;
+
+
+
 }
 - (IBAction)sliderAction:(UISlider *)sender {
 
     //int num = (int)[NSString stringWithFormat:@"%f", self.sliderForMultiplier.value];
     self.multiplierLabel.text = [NSString stringWithFormat:@"%d", (int)self.sliderForMultiplier.value];
 }
-- (IBAction)segmentAction:(UISegmentedControl *)sender {
 
-    switch ([sender selectedSegmentIndex])
-    {
-        case 0:
 
-            self.operator = @"+";
-            break;
-        case 1:
-            self.operator = @"/";
-            break;
-        case 2:
-            self.operator = @"-";
-            break;
-        case 3:
-            self.operator = @"*";
-            break;
-        default:
-            break;
-    }
-}
+//- (IBAction)segmentAction:(UISegmentedControl *)sender {
+//
+//    switch ([sender selectedSegmentIndex])
+//    {
+//        case 0:
+//
+//            self.operator = @"+";
+//            break;
+//        case 1:
+//            self.operator = @"/";
+//            break;
+//        case 2:
+//            self.operator = @"-";
+//            break;
+//        case 3:
+//            self.operator = @"*";
+//            break;
+//        default:
+//            break;
+//    }
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -66,14 +75,32 @@
     int multiplier = self.multiplierLabel.text.intValue;
 
 
-    if([_operator isEqualToString:@"+"])
-      answer = enteredNumber + multiplier;
-    else if ([_operator isEqualToString:@"/"])
-        answer = enteredNumber /multiplier;
-    else if ([_operator isEqualToString:@"-"])
-                answer = enteredNumber - multiplier;
-    else if ([_operator isEqualToString:@"*"])
-                answer = enteredNumber * multiplier;
+    if(self.operatorLabel.selectedSegmentIndex == 0)
+    {
+              answer = enteredNumber + multiplier;
+    }
+    else if (self.operatorLabel.selectedSegmentIndex == 1)
+    {
+              answer = enteredNumber / multiplier;
+    }
+    else if (self.operatorLabel.selectedSegmentIndex == 2)
+    {
+        answer = enteredNumber - multiplier;
+    }
+    else if (self.operatorLabel.selectedSegmentIndex == 3)
+    {
+
+    }        answer = enteredNumber * multiplier;
+
+//
+//    if([_operator isEqualToString:@"+"])
+//      answer = enteredNumber + multiplier;
+//    else if ([_operator isEqualToString:@"/"])
+//        answer = enteredNumber /multiplier;
+//    else if ([_operator isEqualToString:@"-"])
+//                answer = enteredNumber - multiplier;
+//    else if ([_operator isEqualToString:@"*"])
+//                answer = enteredNumber * multiplier;
 
 
      //answer = enteredNumber * multiplier;
